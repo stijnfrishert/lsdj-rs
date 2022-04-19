3a031b40aa87dde686d8bb1c1c7e15629572004e
Stijn Frishert <stijnfrishert@gmail.com>
2022-04-19 15:28:56 +0200

    SRam::from_file

--- lsdj/src/sram/mod.rs
+++ lsdj/src/sram/mod.rs
@@ -7,7 +7,11 @@ pub mod song;
 use fs::{Filesystem, FilesystemReadError};
 use name::{Name, NameFromBytesError};
 use song::{SongMemory, SongMemoryReadError};
-use std::io::Read;
+use std::{
+    fs::File,
+    io::{self, Read},
+    path::Path,
+};
 use thiserror::Error;
 
 /// The SRAM for a full LSDJ save
@@ -21,7 +25,7 @@ pub struct SRam {
 
 impl SRam {
     /// Parse SRAM from an I/O reader
-    pub fn from_reader<R>(mut reader: R) -> Result<Self, SRamReadError>
+    pub fn from_reader<R>(mut reader: R) -> Result<Self, SRamFromReaderError>
     where
         R: Read,
     {
@@ -33,11 +37,22 @@ impl SRam {
             filesystem,
         })
     }
+
+    /// Parse SRAM from a file (.sav)
+    pub fn from_file<P>(path: P) -> Result<Self, SRamFromFileError>
+    where
+        P: AsRef<Path>,
+    {
+        let file = File::open(path)?;
+        let sram = Self::from_reader(file)?;
+
+        Ok(sram)
+    }
 }
 
 /// An error describing what could go wrong reading [`SRam`] from I/O
 #[derive(Debug, Error)]
-pub enum SRamReadError {
+pub enum SRamFromReaderError {
     // Reading the file system from I/O failed
     #[error("Reading the filesystem failed")]
     Filesystem(#[from] FilesystemReadError),
@@ -45,4 +60,16 @@ pub enum SRamReadError {
     // Reading the working memory song from I/O failed
     #[error("Reading the working memory song failed")]
     WorkingSong(#[from] SongMemoryReadError),
+}
+
+/// An error describing what could go wrong reading [`SRam`] from a file
+#[derive(Debug, Error)]
+pub enum SRamFromFileError {
+    // Reading the file system from I/O failed
+    #[error("Opening the file failed")]
+    File(#[from] io::Error),
+
+    // Reading the working memory song from I/O failed
+    #[error("Reading the SRAM from file failed")]
+    Read(#[from] SRamFromReaderError),
 }
